1932046573b39c3c0a84b8b1648297866a2e28f0
Michael Ho <michaelho3624@gmail.com>
2019-08-03 17:37:32 -0400

    Changed homepage message with a bit of styling

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,19 @@ import Layout from './components/Layout';
 export default () => {
   return (
     <Layout>
-      <div>Hello, Next World!</div>
+      <div className="c-main">Welcome to a React Accordian Website!  Now with Hooks!</div>
+
+      {style()}
     </Layout>
   )
+}
+
+function style() {
+  return <style jsx="true">{`
+    .c-main {
+      text-align: center;
+      font-size: 1.25rem;
+      padding-top: 2rem;
+    }
+  `}</style>
 }
